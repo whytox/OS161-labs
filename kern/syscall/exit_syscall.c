9d7fad1a33e62391a0e11f69aa1036144897f511
whytox <s289902@studenti.polito.it>
2023-08-23 10:59:30 +0000

    exit_syscall broadcast cv change could broacast only if the proc exited with an addition if check

--- kern/syscall/exit_syscall.c
+++ kern/syscall/exit_syscall.c
@@ -3,8 +3,18 @@
 #include <syscall.h>
 #include <current.h>
 #include <thread.h>
+#include <proc.h>
 
 void sys__exit(int exit_code) {
-    curthread->exit_code = exit_code;
+    #if OPT_WAITPID
+
+        lock_acquire(curproc->p_cv_lock);
+        spinlock_acquire(&curproc->p_lock);
+        curthread->exit_code = exit_code;
+        cv_broadcast(curproc->p_cv, curproc->p_cv_lock);
+        spinlock_release(&curproc->p_lock);
+        lock_release(curproc->p_cv_lock);
+
+    #endif
     thread_exit();
 }
